9e33b88fbc1cd4c26c3a0ea762c9b14cddf4dc18
Ashish Jingar <ashish.jingar@oracle.com>
2024-10-26 19:13:14 +0530

    Fixed an issue with Gallery Images

--- src/app/gallery/getImagesFromFolder.js
+++ src/app/gallery/getImagesFromFolder.js
@@ -4,7 +4,7 @@ import path from 'path';
 export function getImagesFromFolder(folder) {
     const directoryPath = path.join(process.cwd(), 'public', folder);
     const files = fs.readdirSync(directoryPath);
-    const images = files.filter(file => /\.(png|jpeg|jpg|gif|svg)$/.test(file)).map(file => `/${folder}/${file}`);
+    const images = files.filter(file => /\.(png|jpeg|jpg|gif|svg)$/.test(file)).map(file => `${folder}/${file}`);
     console.log('Fetched images:', images); 
     return images;
 }
