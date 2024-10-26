30fa0fea4179fcf576ddf7dc0a425270b91a2ba5
Ashish Jingar <ashish.jingar@oracle.com>
2024-10-26 19:36:33 +0530

    Fixed Cars Page

--- src/app/components/CarOverview.js
+++ src/app/components/CarOverview.js
@@ -16,10 +16,21 @@ const CarOverview = ({ carName, descrptionText, imageSrc, reverse, handleDetails
             </div>
             <div className={`flex-1 m-5 ${reverse ? "place-items-start" : "place-items-end"} `}>
                 <div className="relative group">
-                    <img src={imageSrc} width={500} height={500} alt="cars" className="rounded-xl border-[#43B02A] border-x-4 " />
+                    <img src={imageSrc} width={500} height={500} alt="cars" className="rounded-xl border-[#43B02A] border-x-4 " 
+                    style={{
+                        width: "80%",
+                        height: "auto",
+                        objectFit: "cover",
+                        objectPosition: "center",
+                    }}
+                    />
                     <div
                         className="absolute inset-0 bg-[#43B02A] bg-opacity-0 group-hover:bg-opacity-40 flex items-center justify-center transition-opacity duration-300 rounded-xl cursor-pointer"
                         onClick={handleDetailsClick}
+                        style={{
+                          width: "80%",
+                            height: "auto",  
+                        }}
                     >
                         <span
                             className={`text-white text-2xl font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${bebas_neue.className}`}
